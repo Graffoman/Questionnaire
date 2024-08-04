c5dac3858c35516079344192a91e7548db5bd86c
Evgeny <truegraff@mail.ru>
2024-08-04 23:01:46 +0300

    Добавлены опции сериализатора Json

--- Front/Program.cs
+++ Front/Program.cs
@@ -1,6 +1,7 @@
 using Front;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Newtonsoft.Json;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,5 +9,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddBlazorBootstrap();
+builder.Services.AddSingleton(sp => new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto });
 
 await builder.Build().RunAsync();
