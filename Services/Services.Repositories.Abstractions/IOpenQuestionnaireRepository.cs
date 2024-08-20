cccb6f31f1ac63d1ffcd27e36689bc114db1aaa2
Evgeny <truegraff@mail.ru>
2024-08-20 12:12:49 +0300

    Добавлены абстракции репозиториев

---
+++ Services/Services.Repositories.Abstractions/IOpenQuestionnaireRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Services.Repositories.Abstractions
+{
+    public interface IOpenQuestionnaireRepository : IRepository<OpenQuestionnaire>
+    {
+
+    }
+}
---
+++ Services/Services.Repositories.Abstractions/IQuestionnaireSubmitRepository.cs
@@ -0,0 +1,9 @@
+﻿using Domain.Entities;
+
+namespace Services.Repositories.Abstractions
+{
+    public interface IQuestionnaireSubmitRepository : IRepository<QuestionnaireSubmit>
+    {
+
+    }
+}
