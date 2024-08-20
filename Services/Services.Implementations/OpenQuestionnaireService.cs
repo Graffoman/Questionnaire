3ef0d73d6782ac9e2247e0ce7ec238546bfce56e
Evgeny <truegraff@mail.ru>
2024-08-20 12:51:32 +0300

    Реализованы сервисы

---
+++ Services/Services.Implementations/OpenQuestionnaireService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Services.Abstractions;
+using Services.Contracts.OpenQuestionnaireDto;
+using Services.Repositories.Abstractions;
+
+namespace Services.Implementations
+{
+    public class OpenQuestionnaireService : IOpenQuestionnaireService
+    {
+        private readonly IMapper _mapper;
+        private readonly IOpenQuestionnaireRepository _openQuestionnaireRepository;
+
+        public OpenQuestionnaireService(IMapper mapper, IOpenQuestionnaireRepository openQuestionnaireRepository)
+        {
+            _mapper = mapper;
+            _openQuestionnaireRepository = openQuestionnaireRepository;
+        }
+
+        public async Task<string> CreateAsync(CreateOpenQuestionnaireDto createOpenQuestionnaireDto)
+        {
+            var openQuestionnaire = _mapper.Map<OpenQuestionnaire>(createOpenQuestionnaireDto);
+            await _openQuestionnaireRepository.AddAsync(openQuestionnaire, CancellationToken.None);
+            return openQuestionnaire.Id;
+        }
+
+        public Task<bool> DeleteByIdAsync(string id)
+        {
+            return _openQuestionnaireRepository.DeleteAsync(id, CancellationToken.None);
+        }
+
+        public Task<List<OpenQuestionnaire>> GetAllAsync()
+        {
+            return _openQuestionnaireRepository.GetAllAsync(CancellationToken.None);
+        }
+
+        public Task<OpenQuestionnaire> GetByIdAsync(string id)
+        {
+            return _openQuestionnaireRepository.GetAsync(id, CancellationToken.None);
+        }
+
+        public Task<bool> UpdateAsync(OpenQuestionnaire openQuestionnaire)
+        {
+            return _openQuestionnaireRepository.UpdateAsync(openQuestionnaire, CancellationToken.None);
+        }
+    }
+}
---
+++ Services/Services.Implementations/QuestionnaireSubmitService.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Services.Abstractions;
+using Services.Contracts.QuestionnaireSubmit;
+using Services.Repositories.Abstractions;
+
+namespace Services.Implementations
+{
+    public class QuestionnaireSubmitService : IQuestionnaireSubmitService
+    {
+        private readonly IMapper _mapper;
+        private readonly IQuestionnaireSubmitRepository _questionnaireSubmitRepository;
+
+        public QuestionnaireSubmitService(IMapper mapper, IQuestionnaireSubmitRepository questionnaireSubmitRepository)
+        {
+            _mapper = mapper;
+            _questionnaireSubmitRepository = questionnaireSubmitRepository;
+        }
+
+        public async Task<string> CreateAsync(CreateQuestionnaireSubmitDto createQuestionnaireSubmitDto)
+        {
+            var questionnaireSubmit = _mapper.Map<QuestionnaireSubmit>(createQuestionnaireSubmitDto);
+            await _questionnaireSubmitRepository.AddAsync(questionnaireSubmit, CancellationToken.None);
+            return questionnaireSubmit.Id;
+        }
+
+        public Task<bool> DeleteByIdAsync(string id)
+        {
+            return _questionnaireSubmitRepository.DeleteAsync(id, CancellationToken.None);
+        }
+
+        public Task<List<QuestionnaireSubmit>> GetAllAsync()
+        {
+            return _questionnaireSubmitRepository.GetAllAsync(CancellationToken.None);
+        }
+
+        public Task<QuestionnaireSubmit> GetByIdAsync(string id)
+        {
+            return _questionnaireSubmitRepository.GetAsync(id, CancellationToken.None);
+        }
+
+        public Task<bool> UpdateAsync(QuestionnaireSubmit questionnaireSubmit)
+        {
+            return _questionnaireSubmitRepository.UpdateAsync(questionnaireSubmit, CancellationToken.None);
+        }
+    }
+}
