19a69dcfaa05e11ba80623091d0f7467a92c02a4
Evgeny <truegraff@mail.ru>
2024-08-20 11:54:40 +0300

    Добавлена сущность OpenQuestionnaire

---
+++ Domain/OpenQuestionnaire.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities.BaseClasses;
+
+namespace Domain.Entities
+{
+    public class OpenQuestionnaire
+    {
+        public string Id { get; set; }
+        public User Respondent { get; set; }
+        public string QuestionnaireRunId { get; set; }
+        public DateTime FinalDateToSubmit { get; set; }
+        public List<Question> Questions { get; set; }
+        public bool Submitted { get; set; } = false;
+    }
+}
