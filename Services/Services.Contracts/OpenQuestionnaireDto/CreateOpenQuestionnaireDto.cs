3f50124da83e9a8bb4740c4985030c802a8a47e7
Evgeny <truegraff@mail.ru>
2024-08-20 12:26:44 +0300

    Добавлены DTO

---
+++ Services/Services.Contracts/OpenQuestionnaireDto/CreateOpenQuestionnaireDto.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.BaseClasses;
+using Domain.Entities;
+
+namespace Services.Contracts.OpenQuestionnaireDto
+{
+    public class CreateOpenQuestionnaireDto
+    {
+        public User Respondent { get; set; }
+        public string QuestionnaireRunId { get; set; }
+        public DateTime FinalDateToSubmit { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+}
---
+++ Services/Services.Contracts/QuestionnaireSubmit/CreateQuestionnaireSubmitDto.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities.BaseClasses;
+using Domain.Entities;
+
+namespace Services.Contracts.QuestionnaireSubmit
+{
+    public class CreateQuestionnaireSubmitDto
+    {
+        public User Respondent { get; set; }
+        public string OpenQuestionnaireId { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+}
