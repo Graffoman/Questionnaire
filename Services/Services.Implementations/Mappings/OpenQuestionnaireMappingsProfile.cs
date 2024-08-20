4873e4f41ccedb162a1bad128d8276cc1bba22df
Evgeny <truegraff@mail.ru>
2024-08-20 12:38:06 +0300

    Добавлены маппинги

---
+++ Services/Services.Implementations/Mappings/OpenQuestionnaireMappingsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Services.Contracts.OpenQuestionnaireDto;
+
+namespace Services.Implementations.Mappings
+{
+    public class OpenQuestionnaireMappingsProfile : Profile
+    {
+        public OpenQuestionnaireMappingsProfile()
+        {
+            CreateMap<CreateOpenQuestionnaireDto, OpenQuestionnaire>()
+                .ForMember(x => x.Id, map => map.Ignore())
+                .ForMember(x => x.Submitted, map => map.MapFrom(src => false));
+        }
+    }
+}
---
+++ Services/Services.Implementations/Mappings/QuestionnaireSubmitMappingsProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Services.Contracts.QuestionnaireSubmit;
+
+namespace Services.Implementations.Mappings
+{
+    public class QuestionnaireSubmitMappingsProfile : Profile
+    {
+        public QuestionnaireSubmitMappingsProfile()
+        {
+            CreateMap<CreateQuestionnaireSubmitDto, QuestionnaireSubmit>()
+                .ForMember(x => x.Id, map => map.Ignore())
+                .ForMember(x => x.SubmitDate, map => map.MapFrom(src => DateTime.Now));
+        }
+    }
+}
