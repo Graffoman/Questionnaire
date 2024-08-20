a9d40986da3062f8194c4fe49af8679ba7fd73be
Evgeny <truegraff@mail.ru>
2024-08-20 12:12:35 +0300

    Добавлены репозитории

---
+++ Infrastructure/Infrastructure.Repositories.Implementations/OpenQuestionnaireRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Infrastructure.DataAcess;
+using Services.Repositories.Abstractions;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class OpenQuestionnaireRepository : Repository<OpenQuestionnaire>, IOpenQuestionnaireRepository
+    {
+        public OpenQuestionnaireRepository(MongoDB<OpenQuestionnaire> db) : base(db)
+        {
+            
+        }
+    }
+}
---
+++ Infrastructure/Infrastructure.Repositories.Implementations/QuestionnaireSubmitRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Infrastructure.DataAcess;
+using Services.Repositories.Abstractions;
+
+namespace Infrastructure.Repositories.Implementations
+{
+    public class QuestionnaireSubmitRepository : Repository<QuestionnaireSubmit>, IQuestionnaireSubmitRepository
+    {
+        public QuestionnaireSubmitRepository(MongoDB<QuestionnaireSubmit> db) : base(db)
+        {
+            
+        }
+    }
+}
