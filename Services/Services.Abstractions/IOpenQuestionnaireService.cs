00cfb36151680bdc6e830f8784345e3a7c568141
Evgeny <truegraff@mail.ru>
2024-08-20 12:27:20 +0300

    Добавлены абстракции сервисов

---
+++ Services/Services.Abstractions/IOpenQuestionnaireService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Services.Contracts.OpenQuestionnaireDto;
+
+namespace Services.Abstractions
+{
+    public interface IOpenQuestionnaireService
+    {
+        Task<List<OpenQuestionnaire>> GetAllAsync();
+        Task<OpenQuestionnaire> GetByIdAsync(string id);
+        Task<string> CreateAsync(CreateOpenQuestionnaireDto createOpenQuestionnaireDto);
+        Task<bool> UpdateAsync(OpenQuestionnaire openQuestionnaire);
+        Task<bool> DeleteByIdAsync(string id);
+    }
+}
---
+++ Services/Services.Abstractions/IQuestionnaireSubmitService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Services.Contracts.QuestionnaireSubmit;
+
+namespace Services.Abstractions
+{
+    public interface IQuestionnaireSubmitService
+    {
+        Task<List<QuestionnaireSubmit>> GetAllAsync();
+        Task<QuestionnaireSubmit> GetByIdAsync(string id);
+        Task<string> CreateAsync(CreateQuestionnaireSubmitDto createQuestionnaireSubmitDto);
+        Task<bool> UpdateAsync(QuestionnaireSubmit questionnaireSubmit);
+        Task<bool> DeleteByIdAsync(string id);
+    }
+}
