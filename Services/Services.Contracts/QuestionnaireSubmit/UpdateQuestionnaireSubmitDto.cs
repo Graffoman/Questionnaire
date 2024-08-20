71876e4613a5040aff48ac562fa9602b185c23e4
Evgeny <truegraff@mail.ru>
2024-08-20 19:45:03 +0300

    Добавлен dto

---
+++ Services/Services.Contracts/QuestionnaireSubmit/UpdateQuestionnaireSubmitDto.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities.BaseClasses;
+using Domain.Entities;
+
+namespace Services.Contracts.QuestionnaireSubmit
+{
+    public class UpdateQuestionnaireSubmitDto
+    {
+        public User Respondent { get; set; }
+        public string OpenQuestionnaireId { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+}
