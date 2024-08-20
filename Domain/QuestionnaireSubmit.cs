c369028dfbd286b7640d5aecdec2693c1b08afe2
Evgeny <truegraff@mail.ru>
2024-08-20 11:57:03 +0300

    Добавлена новая сущность QuestionnaireSubmit

---
+++ Domain/QuestionnaireSubmit.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities.BaseClasses;
+
+namespace Domain.Entities
+{
+    public class QuestionnaireSubmit
+    {
+        public string Id { get; set; }
+        public User Respondent { get; set; }
+        public string OpenQuestionnaireId { get; set; }
+        public DateTime SubmitDate { get; set; }
+        public List<Question> Questions { get; set; }
+    }
+}
