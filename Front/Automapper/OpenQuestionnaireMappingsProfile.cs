a6dfe290f244c450501093bc5860866fd5eb6825
Evgeny <truegraff@mail.ru>
2024-08-29 23:28:35 +0300

    Добавлен новый mapping

---
+++ Front/Automapper/OpenQuestionnaireMappingsProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using Services.Contracts.OpenQuestionnaireDto;
+using Services.Contracts.QuestionnaireSubmitDto;
+
+namespace Front.Automapper
+{
+	public class OpenQuestionnaireMappingsProfile : Profile
+	{
+        public OpenQuestionnaireMappingsProfile()
+        {
+            CreateMap<OpenQuestionnaire, CreateQuestionnaireSubmitDto>()
+                .ForMember(x => x.OpenQuestionnaireId, map => map.MapFrom(src => src.Id));
+
+            CreateMap<OpenQuestionnaire, UpdateQuestionnaireSubmitDto>()
+                .ForMember(x => x.OpenQuestionnaireId, map => map.MapFrom(src => src.Id));
+
+            CreateMap<OpenQuestionnaire, UpdateOpenQuestionnaireDto>();
+        }
+    }
+}
