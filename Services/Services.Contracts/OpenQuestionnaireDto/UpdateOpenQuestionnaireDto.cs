872b12d3147cafdb2a2581f2c1679988bd743eeb
Evgeny <truegraff@mail.ru>
2024-08-30 00:21:13 +0300

    Добавлено новое DTO

---
+++ Services/Services.Contracts/OpenQuestionnaireDto/UpdateOpenQuestionnaireDto.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities.BaseClasses;
+using Domain.Entities;
+
+namespace Services.Contracts.OpenQuestionnaireDto
+{
+	public class UpdateOpenQuestionnaireDto
+	{
+		public string Name { get; set; }
+		public User Respondent { get; set; }
+		public string QuestionnaireRunId { get; set; }
+		public DateTime FinalDateToSubmit { get; set; }
+		public List<Question> Questions { get; set; }
+		public bool Submitted { get; set; }
+	}
+}
