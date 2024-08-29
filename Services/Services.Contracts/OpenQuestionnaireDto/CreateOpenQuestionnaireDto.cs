8a26550110ac273f7de16ea40b6c3f88fbdcba89
Evgeny <truegraff@mail.ru>
2024-08-30 00:20:56 +0300

    Добавлено поле Name

--- Services/Services.Contracts/OpenQuestionnaireDto/CreateOpenQuestionnaireDto.cs
+++ Services/Services.Contracts/OpenQuestionnaireDto/CreateOpenQuestionnaireDto.cs
@@ -5,6 +5,7 @@ namespace Services.Contracts.OpenQuestionnaireDto
 {
     public class CreateOpenQuestionnaireDto
     {
+        public string Name { get; set; }
         public User Respondent { get; set; }
         public string QuestionnaireRunId { get; set; }
         public DateTime FinalDateToSubmit { get; set; }
