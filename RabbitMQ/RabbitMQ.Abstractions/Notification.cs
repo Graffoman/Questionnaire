107186ac26113da950fb2dfb673abde54e8a772d
Evgeny <truegraff@mail.ru>
2024-09-01 01:30:16 +0300

    Добавлена модель уведомления

---
+++ RabbitMQ/RabbitMQ.Abstractions/Notification.cs
@@ -0,0 +1,9 @@
+﻿namespace RabbitMQ.Abstractions
+{
+    public class Notification
+    {
+        public string UserId { get; set; }
+        public string OpenQuestionnaireUrl { get; set; }
+        public string MessageText { get; set; }
+    }
+}
