8d7c59e38e81707043cba48b456d4907edb3dbab
Evgeny <truegraff@mail.ru>
2024-09-05 21:41:08 +0300

    Добавлено поле Email

--- Domain/User.cs
+++ Domain/User.cs
@@ -9,5 +9,6 @@ namespace Domain.Entities
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Department { get; set; }
+        public string Email { get; set; }
     }
 }
--- RabbitMQ/RabbitMQ.Abstractions/Notification.cs
+++ RabbitMQ/RabbitMQ.Abstractions/Notification.cs
@@ -5,5 +5,6 @@
         public string UserId { get; set; }
         public string OpenQuestionnaireUrl { get; set; }
         public string MessageText { get; set; }
+        public string Email { get; set; }
     }
 }
