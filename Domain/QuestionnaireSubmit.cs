34be662aa16168a0c30cac8fa394dc1054e69ff8
Evgeny <truegraff@mail.ru>
2024-09-06 01:06:00 +0300

    Добавлен OpenQuestionnaireName

--- Domain/QuestionnaireSubmit.cs
+++ Domain/QuestionnaireSubmit.cs
@@ -8,6 +8,7 @@ namespace Domain.Entities
         public string Id { get; set; }
         public User Respondent { get; set; }
         public string OpenQuestionnaireId { get; set; }
+        public string OpenQuestionnaireName { get; set; }
         public DateTime SubmitDate { get; set; }
         public List<Question> Questions { get; set; }
     }
--- Front/Automapper/OpenQuestionnaireMappingsProfile.cs
+++ Front/Automapper/OpenQuestionnaireMappingsProfile.cs
@@ -10,10 +10,12 @@ namespace Front.Automapper
         public OpenQuestionnaireMappingsProfile()
         {
             CreateMap<OpenQuestionnaire, CreateQuestionnaireSubmitDto>()
-                .ForMember(x => x.OpenQuestionnaireId, map => map.MapFrom(src => src.Id));
+                .ForMember(x => x.OpenQuestionnaireId, map => map.MapFrom(src => src.Id))
+                .ForMember(x => x.OpenQuestionnaireName, map => map.MapFrom(src => src.Name));
 
             CreateMap<OpenQuestionnaire, UpdateQuestionnaireSubmitDto>()
-                .ForMember(x => x.OpenQuestionnaireId, map => map.MapFrom(src => src.Id));
+                .ForMember(x => x.OpenQuestionnaireId, map => map.MapFrom(src => src.Id))
+                .ForMember(x => x.OpenQuestionnaireName, map => map.MapFrom(src => src.Name));
 
             CreateMap<OpenQuestionnaire, UpdateOpenQuestionnaireDto>();
         }
